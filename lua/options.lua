36503ec0e3ee3ba5f7e48035c7e1c9bb849cb6e6
Elvis Hernandez <elvis.hernandez@kandji.io>
2025-01-14 17:04:47 -0500

    comment out folds stuff (folds weren't saving/loading correctly for some reason)

--- lua/options.lua
+++ lua/options.lua
@@ -68,12 +68,12 @@ vim.o.shiftwidth = 4
 -- vim: ts=2 sts=2 sw=2 et
 
 -- Saves buffer metadata about cursor location and folds to /Users/elvishernandez/.local/state/nvim/view
-vim.opt.viewoptions = 'cursor,folds' -- Save cursor position and folds
-vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
-  pattern = { '*.*' },
-  command = 'silent! mkview',
-})
-vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-  pattern = { '*.*' },
-  command = 'silent! loadview',
-})
+-- vim.opt.viewoptions = 'cursor,folds' -- Save cursor position and folds
+-- vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
+--   pattern = { '*.*' },
+--   command = 'silent! mkview',
+-- })
+-- vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+--   pattern = { '*.*' },
+--   command = 'silent! loadview',
+-- })
